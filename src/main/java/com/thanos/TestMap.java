78936bda6f64b6f6d10aa207b8ba13d9264fc686
Z003ZDBC <shiqiang.yu@siemens.com>
2018-10-25 14:36:00 +0800

    TestMap

---
+++ src/main/java/com/thanos/TestMap.java
@@ -0,0 +1,22 @@
+package com.thanos;
+
+import com.thanos.model.O2OBanner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Thanos Yu
+ * @date 10/25/2018 2:31 PM
+ */
+public class TestMap {
+
+    public static void main(String[] args) {
+        Map<String,O2OBanner> map = new HashMap<>();
+        O2OBanner banner = new O2OBanner();
+        map.put("key",banner);
+        System.out.println("----------------: "+map.get("key").getName());
+        banner.setName("kaka");
+        System.out.println("----------------: "+map.get("key").getName());
+    }
+}
